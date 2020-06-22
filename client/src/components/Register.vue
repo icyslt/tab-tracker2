148eca241412e58256fa60f63b71bbda34e3671f
icyslt <nicolas.garnaud@live.fr>
2020-06-22 15:42:39 +0200

    first video lesson done

---
+++ client/src/components/Register.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+	  <h1>Register</h1>
+
+	  <input
+	  	type="email"
+		name="email"
+		v-model="email"
+		placeholder="email">
+
+	<input
+		type="password"
+		v-model="password"
+		name="password">
+
+	<button
+		@click="register"
+		>Register</button>
+  </div>
+</template>
+
+<script lang="ts">
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+	data() {
+		return {
+			email: '',
+			password: ''
+		}
+	},
+	methods: {
+		async register () {
+			const response = await AuthenticationService.register({
+				email: this.email,
+				password: this.password
+			})
+			console.log(response.data)
+		}
+	},
+}
+/*
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+*/
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Register from '@/components/Register'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+ }, {
+	 path: '/register',
+	 name: 'register',
+	 component: Register
+ }
 ]
 
 const router = new VueRouter({
